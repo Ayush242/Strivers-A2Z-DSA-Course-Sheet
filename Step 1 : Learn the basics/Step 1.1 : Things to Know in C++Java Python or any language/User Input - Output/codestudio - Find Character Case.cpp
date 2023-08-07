6b54cab62c6dc9cb2181416fd5829b279adc8306
Ayush Mittal <45912958+Ayush242@users.noreply.github.com>
2023-08-07 16:44:28 +0530

    Update codestudio - Find Character Case.cpp

--- Step 1 : Learn the basics/Step 1.1 : Things to Know in C++Java Python or any language/User Input - Output/codestudio - Find Character Case.cpp
+++ Step 1 : Learn the basics/Step 1.1 : Things to Know in C++Java Python or any language/User Input - Output/codestudio - Find Character Case.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	// Write your code here
+	char c;
+	cin>>c;
+	if(islower(c)){
+		cout<<0;
+	}
+	else if(isupper(c)){
+		cout<< 1;
+	}
+	else{
+		cout<< -1;
+	}
+}
 
