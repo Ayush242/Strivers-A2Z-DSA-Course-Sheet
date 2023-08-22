7cd3afeea959faea52e74f9453a643b3f81d154d
Ayush Mittal <45912958+Ayush242@users.noreply.github.com>
2023-08-22 18:39:47 +0530

    Create Pattern.cpp

---
+++ Step 1 : Learn the basics/Step 1.2 : Build-up Logical Thinking/Patterns GFG/Pattern.cpp
@@ -0,0 +1,35 @@
+     *
+    ***
+   ***** 
+  *******
+ *********
+***********  
+***********
+ *********
+  *******
+   ***** 
+    ***    
+     *
+
+
+void printDiamond(int n) {
+        // code here
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=(n-i);j++){
+                cout<<" ";
+            }
+            for(int j=1;j<=i;j++){
+                cout<<"* ";
+            }
+            cout<<"\n";
+        }
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=(n-i);j++){
+                cout<<" ";
+            }
+            for(int j=1;j<=i;j++){
+                cout<<"* ";
+            }
+            cout<<"\n";
+        }     
+}
