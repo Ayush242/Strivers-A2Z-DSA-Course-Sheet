7cd657303d0d5af611ac59ed9b5b2faa6585b647
Ayush Mittal <45912958+Ayush242@users.noreply.github.com>
2023-08-22 18:10:13 +0530

    Create Pattern 6.cpp

---
+++ Step 1 : Learn the basics/Step 1.2 : Build-up Logical Thinking/Patterns GFG/Pattern 6.cpp
@@ -0,0 +1,16 @@
+1 2 3 4 5
+1 2 3 4
+1 2 3 
+1 2  
+1 
+
+
+void printTriangle(int n) {
+	    // code here
+	    for(int i=n;i>=1;i--){
+	        for(int j=1;j<=i;j++){
+	            cout<<j<<" ";
+	        }
+	        cout<<"\n";
+	    }
+}
