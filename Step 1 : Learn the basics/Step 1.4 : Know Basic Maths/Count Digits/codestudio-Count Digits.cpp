6e882083a78811f68361811424b70264f2fab162
Ayush Mittal <45912958+Ayush242@users.noreply.github.com>
2023-08-25 22:54:58 +0530

    Update codestudio-Count Digits.cpp

--- Step 1 : Learn the basics/Step 1.4 : Know Basic Maths/Count Digits/codestudio-Count Digits.cpp
+++ Step 1 : Learn the basics/Step 1.4 : Know Basic Maths/Count Digits/codestudio-Count Digits.cpp
@@ -11,3 +11,25 @@ int countDigits(int n){
 	}	
 	return count;
 }
+
+
+// Alternative
+int countDigits(int n)
+{
+    // Initializing 'cur' and 'ans'.
+    int cur = n, ans = 0;
+
+    // Iterating while 'cur' > 0.
+    while (cur > 0)
+    {
+        // 'd_0' gives us the digit at 'ones' place decimal notation of 'cur'.
+        int d_0 = cur % 10;
+        if (d_0 != 0)
+        {
+            // Incrementing 'ans' by 1 if current digit divides 'n' evenly.
+            ans += (n % d_0) == 0;
+        }
+        cur /= 10;
+    }
+    return ans;
+}
