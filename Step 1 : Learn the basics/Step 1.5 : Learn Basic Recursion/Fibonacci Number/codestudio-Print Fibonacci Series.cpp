de5891a634d685e8e747fd8e326d35aaeb91dfa7
Ayush Mittal <45912958+Ayush242@users.noreply.github.com>
2023-08-27 23:58:49 +0530

    Create codestudio-Print Fibonacci Series.cpp

---
+++ Step 1 : Learn the basics/Step 1.5 : Learn Basic Recursion/Fibonacci Number/codestudio-Print Fibonacci Series.cpp
@@ -0,0 +1,12 @@
+vector<int> generateFibonacciNumbers(int n) {
+    // Write your code here.
+    int a=0,b=1,s=0;
+    vector<int> v;
+    for(int i=0;i<n;i++){
+        v.push_back(a);
+        s=a+b;
+        a=b;
+        b=s;
+    }
+    return v;
+}
